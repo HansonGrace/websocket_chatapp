62a6ddb306482230c930ae5c114359e78965730e
Grace Hanson <hansongrace2004@gmail.com>
2025-06-10 14:46:50 -0400

    Write up sessionhandler

--- src/main/java/com/example/websocket_chatapp/client/MyStompSessionHandler.java
+++ src/main/java/com/example/websocket_chatapp/client/MyStompSessionHandler.java
@@ -1,4 +1,18 @@
 package com.example.websocket_chatapp.client;
 
-public class MyStompSessionHandler {
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+public class MyStompSessionHandler extends StompSessionHandlerAdapter {
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders){
+
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception){
+        exception.printStackTrace();
+    }
 }
