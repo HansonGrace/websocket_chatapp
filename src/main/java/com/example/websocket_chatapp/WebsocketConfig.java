e76db803d2deadd3a663d726522598a9f76618ec
Grace Hanson <hansongrace2004@gmail.com>
2025-06-10 13:59:21 -0400

    fix config class and initialize controller

--- src/main/java/com/example/websocket_chatapp/WebsocketConfig.java
+++ src/main/java/com/example/websocket_chatapp/WebsocketConfig.java
@@ -1,4 +1,26 @@
 package com.example.websocket_chatapp;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-public class WebsocketConfig {
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        //sets up a broker where the /topic is a route the websocket server will broadcast messages to
+        config.enableSimpleBroker("/topic");
+        //sets the prefix for messages going to the control
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    //registers stomp endpoints
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //endpoint for websocket connection
+        //http://localhost:8080/ws
+        registry.addEndpoint("/ws").withSockJS();
+    }
 }
